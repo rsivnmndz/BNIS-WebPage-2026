1e5ef427459dc734bfafd33d22bd57ab14b6ab5e
rsivnmndz <roiseivan@gmail.com>
2026-03-12 11:53:19 +0800

    Update AdminDashboard.php

--- AdminDashboard.php
+++ AdminDashboard.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+// If the user is not logged in, kick them back to the login page
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
